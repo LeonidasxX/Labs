f2dcde55a6feb3bebf9f0a76ad1390c531842f72
Gautam Bhagat <101337016+LeonidasxX@users.noreply.github.com>
2023-03-02 10:20:33 +0530

    Update assignment.c

--- Sem4/AlgoLab/Week4/assignment.c
+++ Sem4/AlgoLab/Week4/assignment.c
@@ -1,84 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<limits.h>
+#include <limits.h>
 
-int min[1000], min_value = INT_MAX;
+int min_values[80], min_value = INT_MAX;
 
-void printCombinations(int size, int arr[size][size], int buf[size], int buf_1[size], int ix)
-{
-	int i, j;
-	
-	if(ix == size)
-	{
+void assignment(int n, int job_matrix[n][n], int sum_matrix[n], int check_matrix[n], int check){
+	if(check == n){
 		int sum = 0, flag = 1;
-		
-		for(i = 0; i < size; i++)
+
+		//checking for non-adjacent unequal values, because i=0 and i=1 can't be there
+		for (int i = 0; i < n-1; ++i)
 		{
-			sum += buf[i];
-		}
-		
-		for(i = 0; i < size - 1; i++)
-		{
-			for(j = i + 1; j < size; j++)
+			for (int j = i+1; j < n; ++j)
 			{
-				if(buf_1[i] == buf_1[j])
+				if (check_matrix[i] == check_matrix[j])
 				{
 					flag = 0;
 					break;
 				}
 			}
 		}
-		
-		if(sum < min_value && flag)
+
+		// taking the sum of the matrix obtained
+		for (int i = 0; i < n; ++i)
 		{
-			for(i = 0; i < size; i++)
+			sum += sum_matrix[i];
+		}
+		
+		// setting the minimum values in a matrix and also the minimum sum
+		if (sum < min_value && flag)
+		{
+			for (int i = 0; i < n; ++i)
 			{
-				min[i] = buf[i];
+				min_values[i] = sum_matrix[i];
 			}
-	
 			min_value = sum;
 		}
 	}
-	
-	else
-	{
-		for(i = 0; i < size; i++)
+	else{
+		//base condition - taking all the values from a matrix recursively
+		for (int i = 0; i < n; ++i)
 		{
-			buf[ix] = arr[i][ix];
-			buf_1[ix] = i;
-		
-			printCombinations(size, arr, buf, buf_1, ix + 1);
+			sum_matrix[check] = job_matrix[i][check];
+			check_matrix[check] = i;
+			assignment(n,job_matrix,sum_matrix,check_matrix,check+1);
 		}
 	}
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-	int i, j, size;
-	
-	printf("Enter the Size of the Square Matrix:\n");
-	scanf("%d", &size);
-	
-	int arr[size][size];
-	
-	printf("Enter Person vs Job Cost Matrix :\n");
-	
-	for(i = 0; i < size; i++)
+	int n;
+	// getting the number of jobs
+	printf("Enter the number of jobs: ");
+	scanf("%d", &n);
+	int job_matrix[n][n]; //creating a job vs cost matrix
+
+	printf("Enter cost for each job: ");
+	for (int i = 0; i < n; ++i)
 	{
-		for(j = 0; j < size; j++)
-			scanf("%d", &arr[i][j]);
+		for (int j = 0; j < n; ++j)
+		{
+			scanf("%d", &job_matrix[i][j]);
+		}
 	}
 
-	int buff[size];
-	int buff_1[size];
-	printCombinations(size, arr, buff, buff_1, 0);
+	int sum_matrix[n]; //sum matrix contains elements whose sum has to be calculated
+	int check_matrix[n]; //check matrix is for checking if the index of the adjacent element is not same
+	assignment(n,job_matrix,sum_matrix,check_matrix,0);
 
-	printf("The Minimum Cost Combination is : ");
-
-	for(j = 0; j < size; j++)
-    	printf("%d ", min[j]);
-
-	printf("\nThe Minimum Cost is : %d\n", min_value);
-
+	printf("Minimum cost combination is: ");
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%d ", min_values[i]);
+	}
+	printf("\n Minimum cost: %d\n", min_value);
 	return 0;
 }
