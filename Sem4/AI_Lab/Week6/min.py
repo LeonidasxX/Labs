78c15577aae7316262dd118f3ada835d250066ef
Gautam Bhagat <101337016+LeonidasxX@users.noreply.github.com>
2023-03-06 10:33:55 +0530

    Create min.py

---
+++ Sem4/AI_Lab/Week6/min.py
@@ -0,0 +1,60 @@
+class Graph:
+	def __init__(self):
+		self.graph = {}
+		self.vertex = 0
+		self.vertices = []
+		self.matrix = []
+
+
+	def add_vertex_list_directed(self, v):
+		if v in self.graph:
+			print("Vertex ", v, " already exists.")
+		else:
+			self.vertex += 1
+			self.graph[v] = []
+
+
+	def add_edge_list_directed_weight(self, v1, v2):
+		if v1 not in self.graph:
+			print("Vertex ", v1, " does not exist.")
+		elif v2 not in self.graph:
+			print("Vertex ", v2, " does not exists.")
+		else:
+			temp = [v2,v2*v2]
+			self.graph[v1].append(temp)
+			temp = [v1,v1*v1]
+			self.graph[v2].append(temp)
+
+
+	def display_directed_weight(self):
+		print("Displaying Directed Weighted Graph...")
+		for i in self.graph:
+			if(len(self.graph[i])>1):
+				print(i, " -> ", self.graph[i][0])
+				print(i, " -> ", self.graph[i][1])
+			else:
+				print(i, " -> ", self.graph[i][0])
+		print(self.graph)
+
+	def hill_climb(self):
+		minima = 0
+		for i in self.graph:
+			neighbors = self.graph[i]
+			neighbors_value = []
+			for i in neighbors:
+				neighbors_value.append(i[1])
+			best_neighbor = min(neighbors_value)
+			if minima>best_neighbor:
+				minima = best_neighbor
+		return minima			
+
+
+graph = Graph()
+for i in range(-10,11):
+	graph.add_vertex_list_directed(i)
+
+for i in range(-10,10):
+	graph.add_edge_list_directed_weight(i, i+1)
+
+minima = graph.hill_climb()
+print("Minima of the function x**2 is: ",minima)
